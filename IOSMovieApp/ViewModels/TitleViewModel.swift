dd966b51773f81504e436d9b3ae65539b71ece6f
Berkay Emre Aslan <emreaslan1177@gmail.com>
2025-05-23 20:26:23 +0300

    Building Upcoming View

---
+++ IOSMovieApp/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  IOSMovieApp
+//
+//  Created by Berkay Emre Aslan on 23.05.2025.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
